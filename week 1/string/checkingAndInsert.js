310af2bdd9e7149c1e83b20c47e710cc2fd41e7e
Abin273 <abinvarghese273@gmail.com>
2023-11-24 02:16:47 +0530

    corrected cheeckingAndInsert

--- week 1/string/checkingAndInsert.js
+++ week 1/string/checkingAndInsert.js
@@ -1,26 +1,32 @@
-function s(str,changeAfter,word){
-    let newStr=""
-    let compare=""
-    for(let i=0;i<str.length;i++){
-    
-        if(str[i]!=" "){
-            compare+=str[i];
-            newStr+=str[i];
-        }else{
-            if(changeAfter==compare){
-                newStr+=compare+" ";
-            }else{
-                newStr+=" "+word;
+function changeString(str, changeAfter, word) {
+    let newStr = "";
+    let compare = "";
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] != " ") {
+            compare += str[i];
+            newStr += str[i];
+        } else {
+            if (changeAfter === compare) {
+                newStr += " " + word;
+                compare = ""
+                break;
+            } else {
+                newStr += " ";
+                compare = "";
             }
         }
-
     }
-    return newStr;
 
+    // Check for the last word in the string 
+    if (changeAfter === compare) {
+        newStr += " " + word;
+    }
+
+    return newStr;
 }
 
-
-let str="my name is abin"
-let changeAfter="is";
-let word="hi"
-console.log(s(str,changeAfter,word));
+let str = "my name is abin";
+let changeAfter = "abin";
+let word = "hi";
+console.log(changeString(str, changeAfter, word));
