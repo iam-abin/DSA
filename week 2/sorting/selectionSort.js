bfdd43916094919dc8af1344fe7ab1f86139ff37
Abin273 <abinvarghese273@gmail.com>
2023-11-28 17:39:07 +0530

    selection sort edited

--- week 2/sorting/selectionSort.js
+++ week 2/sorting/selectionSort.js
@@ -1,8 +1,8 @@
 function selectionSort(arr) {
-    for (let i = 0; i < arr.length; i++) {
+    for (let i = 0; i < arr.length-1; i++) {
         let minIndex = i
         for (let j = i + 1; j < arr.length; j++) {
-            if (arr[i] > arr[j]) {
+            if (arr[j] < arr[minIndex]) {
                 minIndex = j
             }
         }
